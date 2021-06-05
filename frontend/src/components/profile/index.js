7258ce1495b28a734634c7b3120212a3504caa56
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-06-05 08:05:59 +0300

    video call still need some work

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-import { If, Then, Else } from 'react-if';
+// import { If, Then, Else } from 'react-if';
 
 const Profile = () => {
 	const state = useSelector((state) => {
