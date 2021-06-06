2f1710b7998fa8b98485ff08d7e219470746b23c
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-06-06 08:59:04 +0300

    arrange the components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,36 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //components
+import Header from './components/header';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
-import Header from './components/header';
-import Footer from './components/footer';
 import Profile from './components/profile';
-import Video from './components/videoCall';
+
+import Navigation from './components/navigation';
 import Questions from './components/questions';
-import Posts from './components/posts';
+import Community from './components/community';
+import Entrance from './components/entrance';
+import Cafe from './components/cafe';
+
+import Footer from './components/footer';
 
 const App = () => {
 	return (
 		<div>
 			<Router>
 				<Header />
+				<br />
+				<Navigation />
 				<Switch>
-					<Route exact path="/login" component={Login} />
+					<Route exact path="/" component={Login} />
 					<Route exact path="/register" component={Register} />
 					<Route exact path="/profile" component={Profile} />
-					<Route exact path="/call" component={Video} />
-					<Route exact path="/posts" component={Posts} />
+					<Route exact path="/questions" component={Questions} />
+					<Route exact path="/community" component={Community} />
+					<Route exact path="/BookTable" component={Entrance} />
+					<Route exact path="/cafe" component={Cafe} />
 				</Switch>
 			</Router>
-			{/* <Questions /> */}
 			<Footer />
 		</div>
 	);
--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -3,12 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { If, Else, Then } from 'react-if';
 import cookie from 'react-cookies';
 import jwt from 'jsonwebtoken';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { login, logout } from './../../reducers/login';
 
 const Header = () => {
 	const dispatch = useDispatch();
-	const history = useHistory();
 
 	const state = useSelector((state) => {
 		return {
@@ -27,7 +26,6 @@ const Header = () => {
 			const user = jwt.decode(token);
 			if (user) {
 				dispatch(login({ user, token }));
-				history.push('/');
 			}
 		} catch (error) {
 			dispatch(login({ user: '', token: '' }));
@@ -41,16 +39,15 @@ const Header = () => {
 	return (
 		<If condition={state.user}>
 			<Then>
-				<span>header</span> <Link to="/profile">{state.user.username}</Link>
+				<span>Logo</span>
+				<Link to="/profile">{state.user.username}</Link>
 				<Link to="/" onClick={logoutHandle}>
 					logout
 				</Link>
-				<Link to="/call">Video</Link>
-				<Link to="/posts">Posts</Link>
 			</Then>
 			<Else>
-				<span>header</span>
-				<Link to="/login">login</Link>
+				<span>Logo</span>
+				<Link to="/">login</Link>
 				<Link to="/register">register</Link>
 			</Else>
 		</If>
