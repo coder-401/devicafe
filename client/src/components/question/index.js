23229be5072cb944bfa3f002bcf0614b4fbe4e94
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-06-07 22:42:46 +0300

    fixed a bug

--- client/src/components/question/index.js
+++ client/src/components/question/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { If, Then } from 'react-if';
 
 const Question = ({ Question }) => {
 	const { question, answer, difficulty, category } = Question;
+	const [show, setShow] = useState(false);
+
+	const handleShow = () => {
+		setShow(!show);
+	};
 
 	return (
 		<div>
 			<p className="qu">{question}</p>
-			<p className="answer">{answer}</p>
+			<div onClick={handleShow} style={{ cursor: 'pointer' }}>
+				Show Answer
+			</div>
+			<If condition={show}>
+				<Then>
+					<p className="answer">{answer}</p>
+				</Then>
+			</If>
 			<p className="category">{category}</p>
 			<p className="difficulty">{difficulty}</p>
 		</div>
