55ef7fc99b143812a09ab4babbd27effcd346ee4
motasemAlsqoor <motasem900khaled@gmail.com>
2021-06-08 02:55:56 +0300

    login and register style

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,38 +1,83 @@
-import React from 'react';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Form, Button, Card } from "react-bootstrap";
+import "./login.css";
 
 const Register = () => {
-	const history = useHistory();
+  const history = useHistory();
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			await axios.post('http://localhost:5000/register', {
-				username: e.target.username.value,
-				email: e.target.email.value,
-				password: e.target.password.value,
-			});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/register", {
+        username: e.target.username.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      });
 
-			history.push('/login');
-		} catch (error) {
-			toast.error('Failed Registration');
-		}
-	};
+      history.push("/login");
+    } catch (error) {
+      toast.error("Failed Registration");
+    }
+  };
 
-	return (
-		<React.Fragment>
-			<form onSubmit={handleSubmit}>
+  return (
+    <React.Fragment>
+      {/* <form onSubmit={handleSubmit}>
 				<input type="text" placeholder="username here" name="username" />
 				<input type="email" placeholder="email here" name="email" />
 				<input type="password" placeholder="password here" name="password" />
 				<button>Register</button>
 			</form>
-			<ToastContainer />
-		</React.Fragment>
-	);
+			<ToastContainer /> */}
+      <div className="login-form">
+        <Card style={{ width: "18rem", paddingBottom: "8px" }}>
+          <Card.Body>
+            <Card.Title>Register</Card.Title>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <ToastContainer />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Register;
