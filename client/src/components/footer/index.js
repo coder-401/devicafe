ae3343295046cd9c0793b6aff5b22bcf17805112
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-06-08 01:07:37 +0300

    homePage

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import "./footer.scss";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<React.Fragment>
+			<footer className="container" id="footer">
+				<div id="footerWrapper">
+					<nav id="footerNav">
+						<a href=""> Services</a>
+						<a href="">About us</a>
+					</nav>
+					<div className="social-media-links">
+						<a href=""><img src="https://www.pngkey.com/png/full/178-1787134_png-file-svg-github-icon-png.png" alt="github" /></a>
+						<a href=""><img src="https://img.icons8.com/ios/452/facebook-new.png" alt="facebook" /></a>
+						<a href=""><img src="https://toppng.com/uploads/preview/twitter-icon-11549680618gtfxwkkgkr.png" alt="twitter" /></a>
+					</div>
+
+				</div>
+
+				<div id="bottom">
+					<div id="copyright">
+						<a href=""><img src="" alt="logo" /></a>
+						<p>All right reserved. The Backenders,<br/> a cooding team &copy; 2021</p>
+					</div>
+				</div>
+			</footer>
+		</React.Fragment>
+	)
 };
 
 export default Footer;
--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { If, Else, Then } from 'react-if';
 import cookie from 'react-cookies';
 import jwt from 'jsonwebtoken';
-import { Link } from 'react-router-dom';
 import { login, logout } from './../../reducers/login';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
 	const dispatch = useDispatch();
@@ -37,20 +37,46 @@ const Header = () => {
 	}
 
 	return (
-		<If condition={state.user}>
-			<Then>
-				<span>Logo</span>
-				<Link to="/profile">{state.user.username}</Link>
-				<Link to="/" onClick={logoutHandle}>
-					logout
-				</Link>
-			</Then>
-			<Else>
-				<span>Logo</span>
-				<Link to="/login">login</Link>
-				<Link to="/register">register</Link>
-			</Else>
-		</If>
+
+		<React.Fragment>
+			<If condition={state.user}>
+				<Then>
+					<Navbar expand="lg" style={{background:"transparent",borderBlockColor:"transparent", zIndex:"3"}} class="float-right">
+						<Navbar.Brand href="/" style={{ marginRight: '70%', fontSize:"150%", fontWeight:"bold" }}>Mocky</Navbar.Brand>
+						<Container>
+							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+							<Navbar.Collapse id="responsive-navbar-nav">
+
+								<Nav>
+									<Nav.Link href="/questions">QUESTIONS</Nav.Link>
+									<Nav.Link href="/BookTable">GET TABLE</Nav.Link>
+									<NavDropdown title="ACCOUNT" id="collasible-nav-dropdown">
+										<NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+										<NavDropdown.Divider />
+										<NavDropdown.Item href="/" onClick={logoutHandle}>logout</NavDropdown.Item>
+									</NavDropdown>
+								</Nav>
+							</Navbar.Collapse>
+						</Container>
+					</Navbar>
+				</Then>
+				<Else>
+				<Navbar expand="lg" style={{background:"transparent",borderBlockColor:"transparent"}} class="float-right">
+						<Navbar.Brand href="/" style={{ marginRight: '70%' }}>DeveCafe</Navbar.Brand>
+						<Container>
+							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+							<Navbar.Collapse id="responsive-navbar-nav">
+
+								<Nav>
+									<Nav.Link href="/login">LOGIN</Nav.Link>
+									<Nav.Link href="/register">REGISTER</Nav.Link>
+								</Nav>
+							</Navbar.Collapse>
+						</Container>
+					</Navbar>
+				</Else>
+			</If>
+		</React.Fragment>
 	);
 };
 
