b46cd3399c3002ff7696c09cab6fe80e7f8e6a66
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-06-08 14:41:56 +0300

    determine who is gonna be the interviewer and who is the interviewee

--- src/database/models/table.js
+++ src/database/models/table.js
@@ -7,6 +7,10 @@ let Table = new Schema({
 	topic: { type: String, required: true },
 	role: { type: String, required: true },
 	difficulty: { type: String, required: true },
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: 'Users',
+	},
 });
 
 module.exports = mongoose.model('Tables', Table);
