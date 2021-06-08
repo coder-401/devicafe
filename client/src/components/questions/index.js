b43c2b226b8a0f12bd8d420d8cc5742099431d3b
motasemAlsqoor <motasem900khaled@gmail.com>
2021-06-08 05:24:23 +0300

    color

--- client/src/components/questions/index.js
+++ client/src/components/questions/index.js
@@ -8,6 +8,7 @@ import { ToastContainer, toast } from "react-toastify";
 import { getQuestion } from "./../../reducers/questions";
 import cookie from "react-cookies";
 import "./questions.css";
+import { Form, Col, Button } from "react-bootstrap";
 
 const Questions = () => {
   const dispatch = useDispatch();
@@ -69,7 +70,59 @@ const Questions = () => {
     <React.Fragment>
       <If condition={cookie.load("auth")}>
         <Then>
-          <form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit} className="filter-form">
+            <Form.Row className="align-items-center">
+              <Col xs="auto" className="my-1">
+                <Form.Label
+                  className="mr-sm-2"
+                  htmlFor="inlineFormCustomSelect"
+                  srOnly
+                >
+                  Preference
+                </Form.Label>
+                <Form.Control
+                  as="select"
+                  className="mr-sm-2 form-select"
+                  id="inlineFormCustomSelect"
+                  name="topic"
+                  custom
+                >
+                  <option value="">Topic</option>
+                  <option value="javascript">JavaScript</option>
+                  <option value="node">NodeJS</option>
+                  <option value="react">ReactJS</option>
+                </Form.Control>
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Form.Label
+                  className="mr-sm-2"
+                  htmlFor="inlineFormCustomSelect"
+                  srOnly
+                >
+                  Preference
+                </Form.Label>
+                <Form.Control
+                  as="select"
+                  className="mr-sm-2 form-select"
+                  id="inlineFormCustomSelect"
+                  name="difficulty"
+                  custom
+                >
+                  <option value="">Difficulty</option>
+                  <option value="beginner">Still Fresh</option>
+                  <option value="intermidate">Joniur Developer</option>
+                  <option value="advance">Senior Developer</option>
+                </Form.Control>
+              </Col>
+
+              <Col xs="auto" className="my-1">
+                <Button variant="outline-dark" type="submit">
+                  Get your Questions
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+          {/* <Form onSubmit={handleSubmit}>
             <select name="topic">
               <option value="">none</option>
               <option value="javascript">JavaScript</option>
@@ -83,7 +136,7 @@ const Questions = () => {
               <option value="advance">Senior Developer</option>
             </select>
             <button>Get your Questions</button>
-          </form>
+          </Form> */}
           <If condition={state.questions.length}>
             <Then>
               <div className="question-container">
